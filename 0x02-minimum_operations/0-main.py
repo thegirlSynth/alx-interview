d787121526cb7b2c16e2f6df72a0600e74547baa
Cynthia Uche - [Linux] <cynthianuchepro@gmail.com>
2023-10-13 16:55:18 +0100

    Handle edge cases

--- 0x02-minimum_operations/0-main.py
+++ 0x02-minimum_operations/0-main.py
@@ -5,8 +5,8 @@ Main file for testing
 
 minOperations = __import__("0-minoperations").minOperations
 
-n = 4
+n = 13
 print("Min # of operations to reach {} char: {}".format(n, minOperations(n)))
 
-n = 12
+n = 19170307
 print("Min # of operations to reach {} char: {}".format(n, minOperations(n)))
