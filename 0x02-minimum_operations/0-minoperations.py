182d571e58f9337c94b56c98f5d87daf5d251081
Cynthia Uche - [Linux] <cynthianuchepro@gmail.com>
2023-10-14 00:39:59 +0100

    Re-write algorithm

--- 0x02-minimum_operations/0-minoperations.py
+++ 0x02-minimum_operations/0-minoperations.py
@@ -10,38 +10,29 @@ def minOperations(n):
     As the first one was not very efficient in handling large numbers,
     there was a need to refactor the function.
 
-    This approach uses a function find_factors to find all the factors of n,
-    and then uses is_prime to find the ones that are prime numbers.
-
-    In other words, the prime factors of n are first computed, and a sum
-    of those factors are returned.
+    This approach is to compute the prime factors of n,
+    and then return a sum of those factors.
     """
-    factors_list = find_factors(n)
 
-    prime_factors = []
-    for item in factors_list:
-        if is_prime(item):
-            prime_factors.append(item)
-
-    return sum(prime_factors)
+    factors_list = prime_factors(n)
+    return sum(factors_list)
 
 
-def find_factors(n):
-    factors = []  # Initialize an empty list to store the factors
+def prime_factors(n, divisor=2):
+    # Base case
+    if n == 1:
+        return []
 
-    # Loop through numbers from 1 to the square root of n
-    for i in range(1, int(n**0.5) + 1):
-        if n % i == 0:
-            factors.append(i)  # i is a factor
-            factors.append(n // i)  # n // i is also a factor
+    factors = []
+
+    # Find the smallest prime divisor of n
+    while n % divisor != 0:
+        divisor += 1
+
+    # Append the prime factor to the list
+    factors.append(divisor)
+
+    # Recursively find the prime factors of the remaining part
+    factors.extend(prime_factors(n // divisor, divisor))
 
     return factors
-
-
-def is_prime(n):
-    if n <= 1:
-        return False
-    for i in range(2, int(n**0.5) + 1):
-        if n % i == 0:
-            return False
-    return True
