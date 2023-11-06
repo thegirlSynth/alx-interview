e1289413fc4df4189ed4a974957296d3e762c192
Cynthia Uche - [Linux] <cynthianuchepro@gmail.com>
2023-11-06 15:20:31 +0100

    Implement checks for vertical and chorizontal threats

--- 0x05-nqueens/0-nqueens.py
+++ 0x05-nqueens/0-nqueens.py
@@ -1,4 +1,4 @@
-#!/usr/bin/env python3
+#!/usr/bin/python3
 """
 Solves the N-Queens Problem as described in the README.md
 """
@@ -39,23 +39,52 @@ def check_diagonal_threat(board, row, col):
 
 
 def check_vertical_threat(board, row, col):
-    pass
+    """
+    Checks if a queen is a vertical threat to another queen
+    """
+    # Check each row of the chessboard for the given column
+
+    for row_index in range(len(board)):
+        if board[row_index][col]:
+            if row_index != row:
+                return True
+    return False
 
 
 def check_horizontal_threat(board, row, col):
+    """
+    Checks if a queen is a horizontal threat to another queen
+    """
+
+    chess_row = board[row]
+    index = 0
+
+    for index in range(len(chess_row)):
+        if chess_row[index]:
+            if chess_row[index] != col:
+                return True
+        index += 1
+    return False
+
+
+def backtrack():
     pass
 
 
 def place_queen(N):
     chess_board = create_board(N)
+    chess_board[3][0] = 1
 
     for board in chess_board:  # output test
         print(board)
+    # print(len(chess_board))
 
-    # check_diagonal_threat(board, row, col)
-    # check_vertical_threat(board, row, col)
-    # check_horizontal_threat(board, row, col)
+    # is_threat = check_diagonal_threat(board, row, col)
+    # is_threat = check_vertical_threat(chess_board, 3, 3)
+    is_threat = check_horizontal_threat(chess_board, 3, 3)
+    return is_threat
 
 
 # is_threat = True
-place_queen(int(sys.argv[1]))
+val = place_queen(int(sys.argv[1]))
+print(val)
