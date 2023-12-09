1d604a0a95b8c8ad7399d32812860bc30fe7e423
Cynthia Uche - [Linux] <cynthianuchepro@gmail.com>
2023-12-09 23:29:49 +0100

    Correct prime number calculation error

--- 0x0A-primegame/0-prime_game.py
+++ 0x0A-primegame/0-prime_game.py
@@ -36,7 +36,7 @@ def is_prime(num):
     if num < 2:
         return False
 
-    for n in range(2, int(0.5**num) + 1):
+    for n in range(2, int(num**0.5) + 1):
         if num % n == 0:
             return False
 
