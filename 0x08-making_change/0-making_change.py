a6fa0566007236e5b6a5bb6fbbe71a437f66d380
Cynthia Uche - [Linux] <cynthianuchepro@gmail.com>
2023-12-29 21:10:23 +0100

    Refactor code

--- 0x08-making_change/0-making_change.py
+++ 0x08-making_change/0-making_change.py
@@ -10,12 +10,17 @@ def makeChange(coins, total):
     """
     if total <= 0:
         return 0
-
-    INF = float("inf")
-    dp = [0] + [INF] * total
-
-    for coin in coins:
-        for i in range(coin, total + 1):
-            dp[i] = min(dp[i], dp[i - coin] + 1)
-
-    return dp[total] if dp[total] != INF else -1
+    rem = total
+    coins_count = 0
+    coin_idx = 0
+    sorted_coins = sorted(coins, reverse=True)
+    n = len(coins)
+    while rem > 0:
+        if coin_idx >= n:
+            return -1
+        if rem - sorted_coins[coin_idx] >= 0:
+            rem -= sorted_coins[coin_idx]
+            coins_count += 1
+        else:
+            coin_idx += 1
+    return coins_count
