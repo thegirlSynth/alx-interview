925b4b8dba7b794ad2b949c39fd783c3f46c7092
Cynthia Uche <61497064+thegirlSynth@users.noreply.github.com>
2024-01-29 17:29:58 +0100

    Update 0-starwars_characters.js

--- 0x06-starwars_api/0-starwars_characters.js
+++ 0x06-starwars_api/0-starwars_characters.js
@@ -1,20 +1,27 @@
 #!/usr/bin/node
-const util = require('util');
-const request = util.promisify(require('request'));
-const filmID = process.argv[2];
+const request = require('request');
+const api = 'https://swapi-api.hbtn.io/api/films/';
 
-async function starwarsCharacters(filmId) {
-    const endpoint = 'https://swapi-api.hbtn.io/api/films/' + filmId;
-    let response = await (await request(endpoint)).body;
-    response = JSON.parse(response);
-    const characters = response.characters;
+request(api + process.argv[2], (err, res, body) => {
+  if (err) {
+    console.log(err);
+  } else {
+    const chars = JSON.parse(body).characters;
 
-    for (let i = 0; i < characters.length; i++) {
-        const urlCharacter = characters[i];
-        let character = await (await request(urlCharacter)).body;
-        character = JSON.parse(character);
-        console.log(character.name);
-    }
+    getChars(chars, 0);
+  }
+});
+
+function getChars (chars, idx) {
+    request(chars[idx], (err, res, body) => {
+      if (err) {
+        console.log(err);
+      } else {
+        console.log(JSON.parse(body).name);
+        }
+
+        if (idx + 1 < chars.length) {
+          getChars(chars, idx + 1);
+        }
+    });
 }
-
-starwarsCharacters(filmID);
