ce02ea90e29f9c5e30593add044a4899b3b817f7
Cynthia Uche - [Linux] <cynthianuchepro@gmail.com>
2024-01-29 17:52:15 +0100

    Update 0-generator.py

--- 0x03-log_parsing/0-generator.py
+++ 0x03-log_parsing/0-generator.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
 """ Log parsing test """
+
 import random
 from sys import stdout
 from time import sleep
@@ -7,10 +8,15 @@ import datetime
 
 for i in range(10000):
     sleep(random.random())
-    stdout.write("{:d}.{:d}.{:d}.{:d} - [{}] \"GET /projects/260 HTTP/1.1\" {} {}\n".format(
-        random.randint(1, 255), random.randint(1, 255), random.randint(1, 255), random.randint(1, 255),
-        datetime.datetime.now(),
-        random.choice([200, 301, 400, 401, 403, 404, 405, 500]),
-        random.randint(1, 1024)
-    ))
+    stdout.write(
+        '{:d}.{:d}.{:d}.{:d} - [{}] "GET /projects/260 HTTP/1.1" {} {}\n'.format(
+            random.randint(1, 255),
+            random.randint(1, 255),
+            random.randint(1, 255),
+            random.randint(1, 255),
+            datetime.datetime.now(),
+            random.choice([200, 301, 400, 401, 403, 404, 405, 500]),
+            random.randint(1, 1024),
+        )
+    )
     stdout.flush()
